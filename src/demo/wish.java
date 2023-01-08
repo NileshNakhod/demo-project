4964fd28b3ff3ce0225fdc8bede293c01e8c95f2
nnnil <nnnil@nilesh>
2023-01-08 15:16:35 +0530

    [Nilesh]:wish class updated

---
+++ src/demo/wish.java
@@ -0,0 +1,13 @@
+package demo;           //package name
+
+public class wish          //class declaration
+{
+	     //class body
+	
+	public static void main(String []args)    //maim method declaration
+	{
+		   //main method body
+		
+		System.out.println("hi");
+	}
+}
